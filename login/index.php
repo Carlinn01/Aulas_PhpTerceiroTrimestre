8a6e2f29e960f6a4987adb5d24bdeab470ac930a
Carlinn01 <carloseduardodalazenoliveira@gmail.com>
2025-09-15 08:48:43 -0300

    Começo da aula 15/09/2025

---
+++ login/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body class="container m-auto my-5 w-75">
+    <div>
+        <p class="fs-2">Esta é a página principal.</p>
+        <p class="fs-3">Aqui a segurança não é necessária.</p>
+        <a class="fs-3" href="login.php">Fazer login (entrar na área administrativa)</a>
+    </div>
+</body>
+
+</html>
